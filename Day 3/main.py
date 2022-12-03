592e0a8d341722a6af02a193fa3147fa0b46711a
bigdrivernet <you@example.com>
2022-12-03 20:35:29 +0100

    Changed Comment Line 16

--- Day 3/main.py
+++ Day 3/main.py
@@ -13,7 +13,7 @@ BadgeSum = 0
 for i in range(len(Backpacks)//3):
     Badge = set(Backpacks[i*3]).intersection(Backpacks[i*3 + 1])
     Badge = Badge.intersection(Backpacks[i*3 + 2])
-    for Character in Badge: #Ther should only be one item in a set at this point but sets are weird
+    for Character in Badge: #There should only be one item in a set at this point but sets are weird
         BadgeSum += string.ascii_letters.index(Character) + 1
 
 print(BadgeSum)
