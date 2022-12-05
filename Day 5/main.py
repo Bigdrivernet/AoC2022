e407d3d6d254f8b2b3483e57912f6cc32c7a0efe
bigdrivernet <you@example.com>
2022-12-05 07:10:37 +0100

    Day 5

--- Day 5/main.py
+++ Day 5/main.py
@@ -9,7 +9,17 @@ Stacks = [
     ["D", "N", "J", "V", "R", "Z", "H", "Q"],
     ["B", "N", "H", "M", "S"]
 ]
-SecondStacks = Stacks
+SecondStacks = [
+    ["D", "T", "W", "F", "J", "S", "H", "N"],
+    ["H", "R", "P", "Q", "T", "N", "B", "G"],
+    ["L", "Q", "V"],
+    ["N", "B", "S", "W", "R", "Q"],
+    ["N", "D", "F", "T", "V", "M", "B"],
+    ["M", "D", "B", "V", "H", "T", "R"],
+    ["D", "B", "Q", "J"],
+    ["D", "N", "J", "V", "R", "Z", "H", "Q"],
+    ["B", "N", "H", "M", "S"]
+]
 
 Moves = open("Day 5\input.txt").read().split("\n")
 
@@ -30,3 +40,12 @@ Output = ""
 for Stack in Stacks:
     Output += Stack[-1]
 print(Output)
+
+for Move in Moves:
+    for i in range(int(Move[0])):
+        SecondStacks[int(Move[2]) - 1].append(SecondStacks[int(Move[1]) - 1].pop(-(int(Move[0])) + i))
+
+SecondOutput = ""
+for Stack in SecondStacks:
+    SecondOutput += Stack[-1]
+print(SecondOutput)
